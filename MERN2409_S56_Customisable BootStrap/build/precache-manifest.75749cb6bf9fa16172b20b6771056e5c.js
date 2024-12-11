f8b63442066e27c6366f74023efb988caae938a2
Manoj <106075652+manoj8043@users.noreply.github.com>
2024-12-11 20:39:11 +0530

    Add files via upload

---
+++ MERN2409_S56_Customisable BootStrap/build/precache-manifest.75749cb6bf9fa16172b20b6771056e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbc1a44db599a1f48709a3b82baba57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5c95ab444cbd03602a4",
+    "url": "/static/css/main.1a2b1bf7.chunk.css"
+  },
+  {
+    "revision": "459da60c6f0a52657311",
+    "url": "/static/js/2.4108adf5.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "e5c95ab444cbd03602a4",
+    "url": "/static/js/main.4bd00892.chunk.js"
+  },
+  {
+    "revision": "d9255067eef9f997e00f",
+    "url": "/static/js/runtime~main.f8e697fc.js"
+  }
+]);
